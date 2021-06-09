6ea5e4bb6202fe4db5cfb7037e920405beb57cd0
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-06-09 13:29:39 -0400

    commit tree c647060cb3c200910ac3b5e234641d5b8dd4794f

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return ({...state,
+                friends: [
+                  ...state.friends,
+                  action.friend
+                ]}
+            )
+        case "REMOVE_FRIEND":
+            const removefriendindex= state.friends.findIndex(friend=> friend.id===action.id);
+            return (
+                {...state, friends: 
+                    [...state.friends.slice(0, removefriendindex),
+                    ...state.friends.slice(removefriendindex+1, state.friends.length)]
+                }
+            )
+        default:
+            return state
+    }
 }
